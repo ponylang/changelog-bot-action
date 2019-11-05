e0a18336a3d23fb2433d9409c1ac679ba3e16179
Sean T. Allen <sean@seantallen.com>
2019-11-05 13:27:51 -0500

    Fix missing pr url

--- entrypoint.sh
+++ entrypoint.sh
@@ -30,7 +30,7 @@ BASE_BRANCH=$(jq -r '.base.ref' pr.json)
 PULL_REQUEST_TITLE=$(jq -r '.title' pr.json)
 PULL_REQUEST_NUMBER=$(jq -r '.number' pr.json)
 COMMIT_MESSAGE="Update CHANGELOG for PR #${PULL_REQUEST_NUMBER} [skip ci]"
-CHANGELOG_ENTRY="- ${PULL_REQUEST_TITLE} ([PR #${PULL_REQUEST_NUMBER}](${PULL_REQUEST_URL}))"
+CHANGELOG_ENTRY="- ${PULL_REQUEST_TITLE} ([PR #${PULL_REQUEST_NUMBER}](${PR_URL}))"
 
 CHANGELOG_TYPES=$(
   cat pr.json |
