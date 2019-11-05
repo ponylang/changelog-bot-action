2fa12d8a7862430f2f614e6ca989088323c43298
Sean T. Allen <sean@seantallen.com>
2019-11-05 09:15:47 -0500

    Add more informational messages

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ SHA=$(jq '.head_commit.id' "${GITHUB_EVENT_PATH}")
 REPO=$(jq '.repository.full_name' "${GITHUB_EVENT_PATH}")
 
 # Search for merged PR featuring our SHA in our REPO
-echo "Running changelog bot for ${SHA} in ${REPO}"
+echo -e "\e[34mRunning changelog bot for ${SHA} in ${REPO}"
 PR_URL=$(curl -s "https://api.github.com/search/issues?q=is:merged+sha:${SHA}+repo:${REPO}" | jq -r '.items[].pull_request.url')
 
 if [[ -z PR_URL ]]; then
@@ -15,9 +15,11 @@ if [[ -z PR_URL ]]; then
 fi
 
 # We have a PR url, let's fetch it.
+echo -e "\e[34mFetching ${PR_URL}"
 curl -s "${PR_URL}" >> pr.json
 
 # Extract information from pull request
+echo -e "\e[34mExtracting information from PR"
 BASE_BRANCH=$(jq -r '.base.ref' pr.json)
 PULL_REQUEST_TITLE=$(jq -r '.title' pr.json)
 PULL_REQUEST_NUMBER=$(jq -r '.number' pr.json)
@@ -33,6 +35,7 @@ CHANGELOG_TYPES=$(
 )
 # git setup
 # Set up .netrc file with GitHub credentials
+echo -e "\e[34mSetting up GitHub credentials"
 cat <<- EOF > $HOME/.netrc
       machine github.com
       login $GITHUB_ACTOR
@@ -48,18 +51,23 @@ git config --global user.name 'Ponylang Main Bot'
 git config --global user.email 'ponylang.main@gmail.com'
 
 # make sure we are up to date
+echo -e "\e[34mCheckout ${BASE_BRANCH}"
 git checkout "${BASE_BRANCH}"
+echo -e "\e[34mPulling latest changes"
 git pull
 
+echo -e "\e[34mUpdating CHANGELOG.md"
 for CHANGELOG_TYPE in $CHANGELOG_TYPES; do
   CHANGELOG_HEADER="### ${CHANGELOG_TYPE}"
   perl -i -ne "BEGIN{$/ = undef;} s@(${CHANGELOG_HEADER}\s*)@\1${CHANGELOG_ENTRY}\n@; print" CHANGELOG.md
 done
 
 # Add CHANGELOG changes and commit
+echo -e "\e[34mCommiting CHANGELOG.md changes (if any)"
 git add CHANGELOG.md
 git commit -m "$COMMIT_MESSAGE"
 
+echo -e "\e[34mPushing changes (if any)"
 # Now we want to be quiet - don't want to print the GITHUB_TOKEN var.
 set +x
 
